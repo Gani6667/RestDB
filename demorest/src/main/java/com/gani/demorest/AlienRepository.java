bfddbf90e535d4b356f0a3f6e7e52c9f3fc272bb
Ganesh <ganeshgani6667@gmail.com>
2024-05-19 15:53:12 +0530

    -Using Mysql

--- demorest/src/main/java/com/gani/demorest/AlienRepository.java
+++ demorest/src/main/java/com/gani/demorest/AlienRepository.java
@@ -1,52 +1,119 @@
 package com.gani.demorest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlienRepository 
 {
-	List<Alien> aliens;
-	
-	public AlienRepository()
-	{
-		aliens = new ArrayList<>();	
-		
-		Alien a1 = new Alien();
-		a1.setId(101);
-		a1.setName("Ganesh");
-		a1.setPoints(80);
-		
-		Alien a2 = new Alien();
-		a2.setId(102);
-		a2.setName("Gani");
-		a2.setPoints(90);	
-		
-		aliens.add(a1);
-		aliens.add(a2);
-		
-	}
-	public List<Alien> getAliens()
-	{
-		return aliens;
-	}
-	
-	public Alien getAlien(int id)
-	{
-		
-		for(Alien a: aliens)
-		{
+    List<Alien> aliens;
+    Connection con = null;
 
-			if(a.getId()==id)
-				return a;
-		}
-		
-		return new Alien();
-	}
-	public void create(Alien a1) 
-	{
-		// TODO Auto-generated method stub
-		aliens.add(a1);
-		
-	}
+    public AlienRepository()
+    {
+        String url = "jdbc:mysql://localhost:3306/restdb";
+        String username = "root";
+        String password = "6667";
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url,username,password);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 
+    public List<Alien> getAliens()
+    {
+        List<Alien> aliens = new ArrayList<>();
+        String sql = "select * from alien";
+        try 
+        {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                Alien a = new Alien();
+                a.setId(rs.getInt(1));
+                a.setName(rs.getString(2));
+                a.setPoints(rs.getInt(3));
+                aliens.add(a);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return aliens;
+    }
+
+    public Alien getAlien(int id) {
+        String sql = "SELECT * FROM alien WHERE id=?";
+        Alien a = new Alien();
+        try {
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                a.setId(rs.getInt(1));
+                a.setName(rs.getString(2));
+                a.setPoints(rs.getInt(3));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return a;
+    }
+
+    public void create(Alien a1) 
+    {
+        String sql = "insert into alien values(?,?,?)";
+        try {
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, a1.getId());
+            st.setString(2, a1.getName());
+            st.setInt(3, a1.getPoints());
+            st.executeUpdate();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void update(Alien a1) 
+    {
+        String sql = "UPDATE alien SET NAME=?,POINTS=? WHERE id=?";
+        try 
+        {
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, a1.getName());
+            st.setInt(2, a1.getPoints());
+            st.setInt(3, a1.getId());
+            st.executeUpdate();
+        } 
+        catch(Exception e) 
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void delete(int id) 
+    {
+        String sql = "delete from alien where id=?";
+        try 
+        {
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, id);
+            st.executeUpdate();
+        } 
+        catch(Exception e) 
+        {
+            System.out.println(e);
+        }
+    }
 }
--- demorest/src/main/java/com/gani/demorest/AlienResource.java
+++ demorest/src/main/java/com/gani/demorest/AlienResource.java
@@ -1,56 +1,71 @@
 package com.gani.demorest;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("aliens")
 public class AlienResource 
 {
-	
-	AlienRepository repo = new AlienRepository();
-	
-	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public List<Alien> getAliens()
-	{
-		System.out.println("getAlien called..");
-		
-		return repo.getAliens();
-		
-	}
-	
-	/*@GET
-	@Path("alien/102")
-	@Produces(MediaType.APPLICATION_XML)
-	public Alien getAlien()
-	{
-		
-		return repo.getAlien(102);
-	}*/
-	
-	@GET
-	@Path("alien/{id}")
-	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
-	public Alien getAlien(@PathParam("id") int id) 
-	{	   
-	    return repo.getAlien(id);
-	}
-	
-	@POST
-	@Path("alien")
-	public Alien createAlien(Alien a1)
-	{
-		
-		System.out.println(a1);
-		repo.create(a1);
-		
-		return a1;
-		
-	}
+    AlienRepository repo = new AlienRepository();
 
+    @GET
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public List<Alien> getAliens()
+    {
+        System.out.println("getAlien called..");
+        return repo.getAliens();
+    }
+
+    @GET
+    @Path("alien/101")
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Alien getAlien()
+    {
+        return repo.getAlien(101);
+    }
+
+    @POST //CREATION OF RESOURCE or DATA
+    @Path("alien")
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Alien createAlien(Alien a1)
+    {
+        System.out.println(a1);
+        repo.create(a1);
+        return a1;
+    }
+
+    @PUT //updating
+    @Path("alien")
+    //@Consumes(MediaType.APPLICATION_XML)//only to support XML
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Alien updateAlien(Alien a1)
+    {
+        System.out.println(a1);
+        if(repo.getAlien(a1.getId()).getId()==0) {
+            repo.create(a1);
+        } else {
+            repo.update(a1);
+        }
+        return a1;
+    }
+
+    @DELETE
+    // Specify the path with the manually provided ID here
+    @Path("alien/202") // here i am using id(202) to delete the id
+
+    public Alien killAlien() {
+        int id = 202; // Manually provided ID
+        Alien a = repo.getAlien(id);
+        if (a.getId() != 0) {
+            repo.delete(id);
+        }
+        return a;
+    }
 }
+
+
+//http://localhost:8082/demorest/webresources/aliens/alien -------->POST
+//http://localhost:8082/demorest/webresources/aliens -------->GET
+//http://localhost:8082/demorest/webresources/aliens/alien/202 ----> DELETE
+
